6ed6bf9f70a04298b6eca0896afc724f3f512f63
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2025-03-20 16:35:18 +0200

    Add light/dark themes to Alacritty

---
+++ scripts/public-scripts/set-terminal-theme.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+## This script replaces the symbolic link for Alacritty's used theme and force
+## reloads Alacritty's configuration.
+
+# NOTE: If MacOS' System Appearance is dark then `Dark` string is returned.
+SYSTEM_APPEARANCE=$(defaults read -g AppleInterfaceStyle 2> /dev/null)
+
+if [ "$SYSTEM_APPEARANCE" = "Dark" ]
+then
+  ln -fs ~/.config/alacritty/dark_theme.toml ~/.config/alacritty/_active_theme.toml
+else
+  ln -fs ~/.config/alacritty/light_theme.toml ~/.config/alacritty/_active_theme.toml
+fi
+
+# Reload Alacritty's configuration.
+touch ~/.config/alacritty/alacritty.yml
+
