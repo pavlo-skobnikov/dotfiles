3001a420ccb77c87bb778f2cd8b712a1f92fca08
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2025-04-01 12:56:08 +0300

    Update the `vim-anywhere-*` scripts to focus the existing Alacritty window and use a separate tmux session

--- scripts/raycast-scripts/vim-anywhere-empty.sh
+++ scripts/raycast-scripts/vim-anywhere-empty.sh
@@ -2,21 +2,21 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Vim Anywhere: Empty
+# @raycast.title Neovim Anywhere: Empty
 # @raycast.mode silent
 
 # Optional parameters:
 # @raycast.icon 📝
 
 # Documentation:
-# @raycast.description Open a new temporary Alacritty window with an empty Vim buffer to compose text in.
+# @raycast.description Open a new temporary Alacritty window with an empty Neovim buffer to compose text in.
 # @raycast.author paulie
 # @raycast.authorURL https://raycast.com/paulie
 
 # This script is a simplified adaptation of the `vim-anywhere` project by
 # Chris Knadler. REFERENCE: https://github.com/cknadler/vim-anywhere
 #
-# This script quickly opens an Alacritty window with an empty Vim buffer.
+# This script quickly opens an Alacritty window with an empty Neovim buffer.
 # After saving and quitting, the buffer is copied into the system clipboard for
 # further usage.
 
@@ -30,17 +30,18 @@ compose_file="/tmp/vim-anywhere.md"
 # Make the compose file empty, optionally creating it if it doesn't exist.
 > "$compose_file"
 
-# Edit the compose file with Vim.
-alacritty -e /opt/homebrew/bin/nu -c "vim $compose_file" &
+# Focus Alacritty.
+osascript -e 'tell application "Alacritty" to activate'
 
-# Get the PID of the Alacritty process.
-alacritty_pid=$!
+# Create a new tmux window named "vim-anywhere", run Vim with the compose file,
+# and signal completion via tmux wait-for when Neovim exits.
+tmux new-window -n vim-anywhere "nvim $compose_file; tmux wait-for -S vim_finished"
 
-# Wait for the Alacritty process to finish.
-wait $alacritty_pid
+# Wait for the vim_finished signal to ensure Neovim has exited.
+tmux wait-for vim_finished
 
-# Copy the contents of the compose file into the system clipboard.
+# Copy the edited text back to the system clipboard.
 cat "$compose_file" | pbcopy
 
-# Switch back to the previous application.
+# Restore focus to the original application.
 osascript -e "tell application \"$current_app\" to activate"
--- scripts/raycast-scripts/vim-anywhere-with-selection.sh
+++ scripts/raycast-scripts/vim-anywhere-with-selection.sh
@@ -2,21 +2,21 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Vim Anywhere: With Selection
+# @raycast.title Neovim Anywhere: With Selection
 # @raycast.mode silent
 
 # Optional parameters:
 # @raycast.icon 📝
 
 # Documentation:
-# @raycast.description Open a new temporary Alacritty window with an empty Vim buffer to compose text in.
+# @raycast.description Open a new temporary Alacritty window with an empty Neovim buffer to compose text in.
 # @raycast.author paulie
 # @raycast.authorURL https://raycast.com/paulie
 
 # This script is a simplified adaptation of the `vim-anywhere` project by
 # Chris Knadler. REFERENCE: https://github.com/cknadler/vim-anywhere
 #
-# This script quickly opens an Alacritty window with a Vim buffer containing
+# This script quickly opens an Alacritty window with a Neovim buffer containing
 # the currently selected text from another application for editing. After saving
 # and quitting, the buffer is copied into the system clipboard for further
 # usage.
@@ -25,30 +25,31 @@
 current_app=$(osascript -e 'tell application "System Events"' \
                          -e 'copy (name of application processes whose frontmost is true) to stdout' \
                          -e 'end tell')
-# This file will hold the result text edited by Vim.
+# This file will hold the result text edited by Neovim.
 compose_file="/tmp/vim-anywhere.md"
 
 # Use osascript to copy the current selection to the clipboard.
 osascript -e 'tell application "System Events" to keystroke "c" using command down'
 
 # Wait a moment to ensure the clipboard has the copied text.
-sleep 0.15
+sleep 0.1
 
 # Pipes the clipboard contents to the compose file, optionally creating it if it
 # doesn't exist.
 pbpaste > "$compose_file"
 
-# Edit the compose file with Vim.
-alacritty -e /opt/homebrew/bin/nu -c "vim $compose_file" &
+# Focus Alacritty.
+osascript -e 'tell application "Alacritty" to activate'
 
-# Get the PID of the Alacritty process.
-alacritty_pid=$!
+# Create a new tmux window named "vim-anywhere", run Neovim with the compose file,
+# and signal completion via tmux wait-for when Neovim exits.
+tmux new-window -n vim-anywhere "nvim $compose_file; tmux wait-for -S vim_finished"
 
-# Wait for the Alacritty process to finish.
-wait $alacritty_pid
+# Wait for the vim_finished signal to ensure Neovim has exited.
+tmux wait-for vim_finished
 
-# Copy the contents of the compose file into the system clipboard.
+# Copy the edited text back to the system clipboard.
 cat "$compose_file" | pbcopy
 
-# Switch back to the previous application.
+# Restore focus to the original application.
 osascript -e "tell application \"$current_app\" to activate"
