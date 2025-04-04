2926cf2c5c72f34e70d5e3930ac82c594600af43
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2025-04-04 09:59:18 +0300

    Use helix in my Vim anywhere scripts

--- scripts/raycast-scripts/hx-anywhere-empty.sh
+++ scripts/raycast-scripts/hx-anywhere-empty.sh
@@ -2,21 +2,21 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Neovim Anywhere: Empty
+# @raycast.title Helix Anywhere: Empty
 # @raycast.mode silent
 
 # Optional parameters:
-# @raycast.icon 📝
+# @raycast.icon 🧬
 
 # Documentation:
-# @raycast.description Open a new temporary Alacritty window with an empty Neovim buffer to compose text in.
+# @raycast.description Open a new temporary Alacritty window with an empty Helix buffer to compose text in.
 # @raycast.author paulie
 # @raycast.authorURL https://raycast.com/paulie
 
 # This script is a simplified adaptation of the `vim-anywhere` project by
 # Chris Knadler. REFERENCE: https://github.com/cknadler/vim-anywhere
 #
-# This script quickly opens an Alacritty window with an empty Neovim buffer.
+# This script quickly opens an Alacritty window with an empty Helix buffer.
 # After saving and quitting, the buffer is copied into the system clipboard for
 # further usage.
 
@@ -24,8 +24,8 @@
 current_app=$(osascript -e 'tell application "System Events"' \
                          -e 'copy (name of application processes whose frontmost is true) to stdout' \
                          -e 'end tell')
-# This file will hold the result text edited by Vim.
-compose_file="/tmp/vim-anywhere.md"
+# This file will hold the result text edited by Helix.
+compose_file="/tmp/hx-anywhere.md"
 
 # Make the compose file empty, optionally creating it if it doesn't exist.
 > "$compose_file"
@@ -33,12 +33,12 @@ compose_file="/tmp/vim-anywhere.md"
 # Focus Alacritty.
 osascript -e 'tell application "Alacritty" to activate'
 
-# Create a new tmux window named "vim-anywhere", run Vim with the compose file,
-# and signal completion via tmux wait-for when Neovim exits.
-tmux new-window -n vim-anywhere "nvim $compose_file; tmux wait-for -S vim_finished"
+# Create a new tmux window named "hx-anywhere", run hx with the compose file,
+# and signal completion via tmux wait-for when Helix exits.
+tmux new-window -n hx-anywhere "hx $compose_file; tmux wait-for -S hx_finished"
 
-# Wait for the vim_finished signal to ensure Neovim has exited.
-tmux wait-for vim_finished
+# Wait for the hx_finished signal to ensure Helix has exited.
+tmux wait-for hx_finished
 
 # Copy the edited text back to the system clipboard.
 cat "$compose_file" | pbcopy
--- scripts/raycast-scripts/hx-anywhere-with-selection.sh
+++ scripts/raycast-scripts/hx-anywhere-with-selection.sh
@@ -2,21 +2,21 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Neovim Anywhere: With Selection
+# @raycast.title Helix Anywhere: With Selection
 # @raycast.mode silent
 
 # Optional parameters:
-# @raycast.icon 📝
+# @raycast.icon 🧬
 
 # Documentation:
-# @raycast.description Open a new temporary Alacritty window with an empty Neovim buffer to compose text in.
+# @raycast.description Open a new temporary Alacritty window with an empty Helix buffer to compose text in.
 # @raycast.author paulie
 # @raycast.authorURL https://raycast.com/paulie
 
 # This script is a simplified adaptation of the `vim-anywhere` project by
 # Chris Knadler. REFERENCE: https://github.com/cknadler/vim-anywhere
 #
-# This script quickly opens an Alacritty window with a Neovim buffer containing
+# This script quickly opens an Alacritty window with a Helix buffer containing
 # the currently selected text from another application for editing. After saving
 # and quitting, the buffer is copied into the system clipboard for further
 # usage.
@@ -25,8 +25,8 @@
 current_app=$(osascript -e 'tell application "System Events"' \
                          -e 'copy (name of application processes whose frontmost is true) to stdout' \
                          -e 'end tell')
-# This file will hold the result text edited by Neovim.
-compose_file="/tmp/vim-anywhere.md"
+# This file will hold the result text edited by Helix.
+compose_file="/tmp/hx-anywhere.md"
 
 # Use osascript to copy the current selection to the clipboard.
 osascript -e 'tell application "System Events" to keystroke "c" using command down'
@@ -41,12 +41,12 @@ pbpaste > "$compose_file"
 # Focus Alacritty.
 osascript -e 'tell application "Alacritty" to activate'
 
-# Create a new tmux window named "vim-anywhere", run Neovim with the compose file,
-# and signal completion via tmux wait-for when Neovim exits.
-tmux new-window -n vim-anywhere "nvim $compose_file; tmux wait-for -S vim_finished"
+# Create a new tmux window named "hx-anywhere", run Helix with the compose file,
+# and signal completion via tmux wait-for when Helix exits.
+tmux new-window -n hx-anywhere "hx $compose_file; tmux wait-for -S hx_finished"
 
-# Wait for the vim_finished signal to ensure Neovim has exited.
-tmux wait-for vim_finished
+# Wait for the hx_finished signal to ensure Helix has exited.
+tmux wait-for hx_finished
 
 # Copy the edited text back to the system clipboard.
 cat "$compose_file" | pbcopy
