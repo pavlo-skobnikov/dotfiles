bcee72417bd092d9dda8a4db2031481f2387fb9a
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2025-06-09 16:44:16 +0300

    Move theme-related stuff from .zprofile to set-terminal-colors

--- scripts/public-scripts/set-terminal-colors.sh
+++ scripts/public-scripts/set-terminal-colors.sh
@@ -1,20 +1,44 @@
 #!/usr/bin/env bash
 
-## A script to Update Alacritty's current theme based on the system appearance.
 ## A script to dynamically update "themes" for terminal tools when the script is run.
 
+# Bat theme definitions.
+bat_light_theme="Catppuccin Latte"
+bat_dark_theme="Catppuccin Frappe"
+
+# Fzf color definitions.
+fzf_light_theme_style_part=" \
+    --color=bg+:#CCD0DA,bg:#EFF1F5,spinner:#DC8A78,hl:#D20F39 \
+    --color=fg:#4C4F69,header:#D20F39,info:#8839EF,pointer:#DC8A78 \
+    --color=marker:#7287FD,fg+:#4C4F69,prompt:#8839EF,hl+:#D20F39 \
+    --color=selected-bg:#BCC0CC \
+    --color=border:#CCD0DA,label:#4C4F69"
+fzf_dark_theme_style_part=" \
+    --color=bg+:#414559,bg:#303446,spinner:#F2D5CF,hl:#E78284 \
+    --color=fg:#C6D0F5,header:#E78284,info:#CA9EE6,pointer:#F2D5CF \
+    --color=marker:#BABBF1,fg+:#C6D0F5,prompt:#CA9EE6,hl+:#E78284 \
+    --color=selected-bg:#51576D \
+    --color=border:#414559,label:#C6D0F5"
+
+# Variables for theme updates.
+bat_theme=''
 fzf_theme_style_part=''
 
 if [[ -z $(defaults read -g AppleInterfaceStyle 2> /dev/null) ]]; then
     ln -fs ~/.config/alacritty/light_theme.toml ~/.config/alacritty/_active_theme.toml
-    fzf_theme_style_part="$FZF_LIGHT_THEME_STYLE_PART"
+    bat_theme="$bat_light_theme"
+    fzf_theme_style_part="$fzf_light_theme_style_part"
 else
     ln -fs ~/.config/alacritty/dark_theme.toml ~/.config/alacritty/_active_theme.toml
-    fzf_theme_style_part="$FZF_DARK_THEME_STYLE_PART"
+    fzf_theme_style_part="$fzf_dark_theme_style_part"
+    bat_theme="$bat_dark_theme"
 fi
 
 # Force Alacritty to refresh configuration (and, therefore, its colors).
 touch ~/.config/alacritty/alacritty.toml
+
+# Update Bat theme.
+export BAT_THEME="$bat_theme"
 
 # Reapply Fzf default options with a new theme.
 export FZF_DEFAULT_OPTS="$fzf_theme_style_part \
