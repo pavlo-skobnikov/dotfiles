07f931e34561a04288b0c12d1c692219b1cc5f62
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2025-08-04 12:57:56 +0300

    Fix neovim telescope-fzf-native build error

--- configurations/.config/nvim/lua/lazy_plugins/telescope.lua
+++ configurations/.config/nvim/lua/lazy_plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
         'echasnovski/mini.icons',
         {
             'nvim-telescope/telescope-fzf-native.nvim',
-            build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release',
+            build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 && cmake --build build --config Release',
         },
         'nvim-telescope/telescope-ui-select.nvim',
     },
